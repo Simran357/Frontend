4ef0fb7ffc6e5bdf1b41810647901878df1ff080
SIMRAN SHARMA <lovelysharma2535@gmail.com>
2026-06-25 02:20:46 +0530

    j

--- src/Components/Dashboard/Form/Utils/AxiosInstance.jsx
+++ src/Components/Dashboard/Form/Utils/AxiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance
 const axiosInstance = axios.create({
-  baseURL:"https://backend-2-0rfy.onrender.com/api",
+  baseURL:"https://backend-1-ni3c.onrender.com/api",
   timeout: 30000,
   withCredentials: true, // send cookies if needed
 });
